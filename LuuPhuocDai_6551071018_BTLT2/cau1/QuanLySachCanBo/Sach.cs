73d60f8f5ad5397152010baa945028243a71a475
LuuPhuocDai06 <dailuuphuoc@gmail.com>
2026-09-07 08:18:35 +0700

    add btChuong2

---
+++ LuuPhuocDai_6551071018_BTLT2/cau1/QuanLySachCanBo/Sach.cs
@@ -0,0 +1,25 @@
+﻿namespace QuanLySachCanBo
+{
+    internal class Sach
+    {
+        // các thuộc tính.
+        private String maSach;
+        private String tenSach;
+        private String tacGia;
+        private int namXuatSu;
+        private double giaBan;
+
+        // constructor.
+        public Sach(String maSach, String tenSach ,String tacGia, int namXuatSu, double giaBan)
+        {
+            this.maSach = maSach;
+        }
+
+        // Tạo getter và setter.
+        // tenSach chỉ đọc và ghi.
+        
+
+
+
+    }
+}
